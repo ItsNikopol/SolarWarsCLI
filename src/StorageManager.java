219fe211b085f5ac8af60c306af5f28dc862e584
Nikopol <kirillcraft1000@yandex.ru>
2023-03-27 21:53:51 +0400

    Change types

--- src/StorageManager.java
+++ src/StorageManager.java
@@ -1,38 +1,38 @@
 public class StorageManager {
-    short[] Storage = new short[8];
-    short[] Prices = new short[8];
+    int[] Storage = new int[8];
+    int[] Prices = new int[8];
     int money = 0;
     int debt = 0;
     int savings = 0;
     boolean gun = false;
     int capacity = 80;
     void refreshPrices(int mode){
-        Prices[0] = (short) IOManager.rand.nextInt(100,2000);
-        Prices[1] = (short) IOManager.rand.nextInt(5000,30000);
-        Prices[2] = (short) IOManager.rand.nextInt(10,300);
-        Prices[3] = (short) IOManager.rand.nextInt(4000,11000);
-        Prices[4] = (short) IOManager.rand.nextInt(1500,4000);
-        Prices[5] = (short) IOManager.rand.nextInt(100,3000);
+        Prices[0] = IOManager.rand.nextInt(100,2000);
+        Prices[1] = IOManager.rand.nextInt(5000,30000);
+        Prices[2] = IOManager.rand.nextInt(10,300);
+        Prices[3] = IOManager.rand.nextInt(4000,11000);
+        Prices[4] = IOManager.rand.nextInt(1500,4000);
+        Prices[5] = IOManager.rand.nextInt(100,3000);
         if (mode == 4){
             Prices[6] = 0;
         }
         else {
-            Prices[6] = (short) IOManager.rand.nextInt(2000,5000);
+            Prices[6] = IOManager.rand.nextInt(2000,5000);
         }
-        Prices[7] = (short) IOManager.rand.nextInt(100,2000);
+        Prices[7] = IOManager.rand.nextInt(100,2000);
         if (mode == 2){
-            Prices[7] = (short) (Prices[7]*1.2);
+            Prices[7] = (int) (Prices[7] * 1.2);
         }
     }
     int calculateOccupied(){
         int busy = 0;
-        for (byte i = 0; i < 8;i++){
+        for (int i = 0; i < 8;i++){
             busy = busy + Storage[i];
         }
         return busy;
     }
     void printTable(){
-        for (byte i = 0; i < 8;i++){
+        for (int i = 0; i < 8;i++){
             if (Prices[i] == 0) {
                 System.out.println("I"+i+" ["+Storage[i]+"] "+ IOManager.ProductName(i)+" N/A");
             }
@@ -48,16 +48,16 @@ public class StorageManager {
             System.out.println();
         }
     }
-    void add(int item, int ammount, boolean payment){
-        Storage[item] = (short) (Storage[item] + ammount);
+    void add(int item, int amount, boolean payment){
+        Storage[item] = (Storage[item] + amount);
         if (payment){
-            money = money - (Prices[item] * ammount);
+            money = money - (Prices[item] * amount);
         }
     }
-    void remove(int item, int ammount, boolean payment){
-        Storage[item] = (short) (Storage[item] - ammount);
+    void remove(int item, int amount, boolean payment){
+        Storage[item] = (Storage[item] - amount);
         if (payment){
-            money = money + (Prices[item] * ammount);
+            money = money + (Prices[item] * amount);
         }
     }
     void clear(){
