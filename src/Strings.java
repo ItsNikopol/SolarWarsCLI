92c689611ab72f55ecb68e0f9460793c342c8589
ItsNikopol <kirillcraft1000@yandex.ru>
2023-04-15 10:56:37 +0400

    New Strings

--- src/Strings.java
+++ src/Strings.java
@@ -1,14 +1,25 @@
 public class Strings {
+	//Items
     static String[] PlanetName = {"", "Earth", "Mars", "Jupiter", "Saturn", "Neptune", "Pluto"};
     static String[] ProductName = {"Fuel", "Dilithum", "Holos", "Ore", "Meds", "Food", "Weapons", "Water"};
     static String[] InputmodesName = {"Select","Menu","Count","Item","Planet","Action","Y/N"};
+	//Main menu
     static String Logo = "   _____       __                                   \n  / ___/____  / /___ _______      ______ ___________\n  \\__ \\/ __ \\/ / __ `/ ___/ | /| / / __ `/ ___/ ___/\n ___/ / /_/ / / /_/ / /   | |/ |/ / /_/ / /  (__  ) \n/____/\\____/_/\\__,_/_/    |__/|__/\\__,_/_/  /____/  ";
     static String Name = "Solarwars v0.9a";
     static String Help = "Game commands:\nBuying:\n[b]/[1] - Buy; [s]/[2] - Sell\nManaging money:\n[bk]/[3] - Bank; [c]/[4] - Corporation\nMoving:\n[w]/[5] - Warp to planet; [ff]/[6] - Skip day\nOther:\n[h] - Help; [q] - Quit from the game.";
     static String Modes = "[1] Normal Mode - 60 Days\n[2] Blitz Mode - 25 Days\n[3] Quit";
+	//Actions
+	static String HelpMessage = "Type [h] to see list of commands.";
+	static String ExitMessage = "Do you really want to exit? Progress won't be saved. [y/n]";
     static String InvalidInput = "Invalid Input.";
+	static String InvalidInputHelp = "Invalid input.\nYou may want to type [h] to see list of commands.";
+	static String InvalidInput = "Invalid Input.";
+	static String FullStorage = "Storage is full.";
+	static String Buy = "What to buy? [0-7]";
+	static String Sell = "What to sell? [0-7]";
+	
+	
     static String PiratesLose = "KABOOOM! Your ship was destroyed.\nPirates captured your ship and took all of your cargo.";
     static String PiratesEscape = "You lost them in the Clouds.";
-
-
+	
 }
