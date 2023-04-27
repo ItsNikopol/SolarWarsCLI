cdbe2d47c686527658de275d9d30df101aff1fd0
Kirill0743 <37414588+Kirill0743@users.noreply.github.com>
2023-04-27 20:34:15 +0300

    Fixing locales

--- src/java/LocaleAdapter.java
+++ src/java/LocaleAdapter.java
@@ -1,24 +1,35 @@
 import com.google.gson.Gson;
 
 
-import java.io.InputStreamReader;
-import java.io.Reader;
+import java.io.IOException;
+import java.io.InputStream;
+
+import java.nio.charset.StandardCharsets;
 import java.util.ListResourceBundle;
 import java.util.Locale;
-import java.util.Objects;
+
 
 @SuppressWarnings("unused")
 public class LocaleAdapter extends ListResourceBundle {
 	Locale locale;
+	Object[][] localeReal;
 
 	public  LocaleAdapter(Locale l){
 		this.locale=l;
+		Gson gson = new Gson();
+		//System.out.println("/locale/" +locale.getLanguage()+"_"+locale.getCountry() + ".json");
+		InputStream localestream = getClass().getResourceAsStream("/locale/" +locale.getLanguage()+"_"+locale.getCountry() +  ".json");
+		byte[] localebuf;
+		try {
+			localebuf = localestream.readAllBytes();
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+		String localestr = new String(localebuf, StandardCharsets.UTF_8);
+		this.localeReal=gson.fromJson(localestr,Object[][].class);
 	}
 
 	protected Object[][] getContents() {
-		Gson gson = new Gson();
-		//System.out.println("/locale/" +locale.getLanguage()+"_"+locale.getCountry() + ".json");
-		Reader reslocale = new InputStreamReader(Objects.requireNonNull(getClass().getResourceAsStream("/locale/" +locale.getLanguage()+"_"+locale.getCountry() +  ".json")));
-		return gson.fromJson(reslocale,Object[][].class);
+		return localeReal;
 	}
 }
